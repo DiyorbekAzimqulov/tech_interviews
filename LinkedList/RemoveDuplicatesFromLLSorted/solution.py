3ef6e85027fac44d65fee5b77c538d000f6db064
AzimqulovDiyorbek <azimqulovdiyorbek@gmail.com>
2022-09-02 08:18:57 +0500

    Remove Duplicates

---
+++ LinkedList/RemoveDuplicatesFromLLSorted/solution.py
@@ -0,0 +1,23 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        if head and head.next is None:
+            return head
+        prev = head
+        cur = head.next
+        while cur is not None:
+            if cur.val == prev.val:
+                cur = cur.next
+            else:
+                prev.next = cur
+                prev = cur
+                cur = cur.next
+        prev.next = None
+        return head
