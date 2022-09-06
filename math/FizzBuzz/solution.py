e81661a6ac282a7add249aa4a36e904d0091d463
AzimqulovDiyorbek <azimqulovdiyorbek@gmail.com>
2022-09-06 22:33:38 +0500

    FizzBuzz

---
+++ math/FizzBuzz/solution.py
@@ -0,0 +1,19 @@
+"""
+https://leetcode.com/problems/fizz-buzz/
+"""
+
+from typing import List
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        result = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append('FizzBuzz')
+            elif i % 5 == 0:
+                result.append('Buzz')
+            elif i % 3 == 0:
+                result.append('Fizz')
+            else:
+                result.append(str(i))
+        return result
