e936f5d6139797e396bf3fe89c4f1642acc79851
AzimqulovDiyorbek <azimqulovdiyorbek@gmail.com>
2022-09-17 12:35:32 +0500

    minimum bracket addition, site:binarysearch

---
+++ stack/MinimumBracketAddition/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solve(self, s):
+        """
+        if we encounter opening bracket we put it to the stack
+        if we encounter closing bracket we check whether we have sufficient opening bracket, if we have then we pop that element, otherwise we need to insert bracket. 
+        in the end if we have more opening brackets than closing ones
+        then we add number of that brackets to the bracket insert count.
+        ))(()())
+        )))((
+        time O(n)
+        space O(1)
+        """
+        map_ = {
+            ")": "("
+        }
+        stack = []
+        add = 0
+        for bracket in s:
+            if bracket not in map_:
+                # opening bracket
+                stack.append(bracket)
+            else:
+                # closing bracket
+                if stack:
+                    stack.pop()
+                else:
+                    add += 1
+        add += len(stack)
+        return add
