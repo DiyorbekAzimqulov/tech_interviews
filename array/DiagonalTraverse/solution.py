2e46982b8946fdf57531669adae76df73a27922e
AzimqulovDiyorbek <azimqulovdiyorbek@gmail.com>
2022-12-24 08:56:53 +0100

    description to diagonal traversal

--- array/DiagonalTraverse/solution.py
+++ array/DiagonalTraverse/solution.py
@@ -6,6 +6,40 @@ class Solution:
         https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1167/
         up - up one row and right one column
         down - down one row and left one column
+        our algorithm runs untill we store all elements in the matrix diagonally to the result array.
+
+Going up 
+
+for going up we first go one row up and one column to the right
+
+while our current row is greater than or equal to 0 and we are not out of columns
+it means we are in the grid
+we append mat[row][col] to the result array
+and decrement row by one for going up 
+and increment column by one for going right
+if column is out of grid it means we need to go back one column and down 2 row
+we add 2 to our row to go down
+and we decrement our column by one for coming to grid
+if our row is less than 0 that means we are outside of grid by row 
+so we need to add 1 to our row to go back to grid
+
+Going down
+
+For going down we first go down by one row and one column to the right 
+
+while our current column is greater than or equal to 0 and we are not out of rows
+it means we are in the grid
+we append mat[row][col] to the result array
+and decrement column by one for going left 
+and increment row by one for going up
+if row is out of grid it means we need to go up one row and  2 column to the right
+we add 2 to our column to go right
+and we decrement our row by one for coming to grid (going up)
+if our column is less than 0 that means we are outside of grid by column
+so we need to add 1 to our column to go back to grid
+
+
+
         """
         
         num_rows = len(mat)
