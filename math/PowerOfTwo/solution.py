c51d602e73b2eea35b32c52a505239d92f5a8f2e
AzimqulovDiyorbek <azimqulovdiyorbek@gmail.com>
2022-12-31 21:20:10 +0100

    power of two

--- math/PowerOfTwo/solution.py
+++ math/PowerOfTwo/solution.py
@@ -23,3 +23,19 @@ class Solution:
         4 & 8 -> 0, 1, 0, 0
         """
         return n and not(n & (n-1))
+
+
+# class Solution:
+#     def isPowerOfTwo(self, n: int) -> bool:
+#         """
+#         we know the power of 2's are in binary is like this:
+#         2^0 = 1 -> 01 & 0 = 0
+#         2^1 = 2 -> 10 & 01 = 0
+#         2^2 = 4 -> 100 & 011 = 
+#         2^3 = 8 -> 1000 & 0111 = 0
+#         2^4 = 16 -> 10000 & 01111 = 
+#         2^5 = 32 -> 100000 & 011111 = 0
+#         so if we make bitwise AND operation with power of two and
+#         one less than this number then we get 0
+#         """
+#         return n > 0 and n & (n-1) == 0
